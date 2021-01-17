bf79971afe4c6c897b18174fa4f86c7c07b96a2c
Ömer Şiranlı <57799108+sekizsehir@users.noreply.github.com>
2021-01-17 23:21:29 +0300

    Initial commit

---
+++ Kampİntro/Program.fs
@@ -0,0 +1,19 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+    0 // return an integer exit code
+
+    using System;
+    
+    class MainClass {
+      public static void Main (string[] args) {
+        //type safety
+       string KatagoriEtiketi = "Kategoir";
+
+       Console.WriteLine ("Kategoir");
+      }
+    }
---
+++ Metotlar/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string urunAdi = "Elma";
+            //double urunFiyati = 15;
+           // string urunAciklama = "Amasya Elamsı";
+
+            string[] meyveler = new string[] { };
+
+            Urun urun1 = new Urun();
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "Amasya Elması";
+
+            Urun urun2 = new Urun();
+            urun2.Adi = "Karpuz";
+            urun2.Fiyati = 80;
+            urun2.Aciklama = "Diyarbakır Karpuzu";
+
+            //type-safe -- tip güvenligi
+            Urun[] urunler = new Urun[] {urun1,urun2};
+
+            foreach (Urun urun in urunler)
+            {
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Fiyati);
+                Console.WriteLine(urun.Aciklama);
+                Console.WriteLine("-----------------");
+
+            }
+
+            Console.WriteLine("-------Metotlar------");
+
+            //instance - Örnek
+            //encapsulation - Kapsülleme
+            sepetManager sepetManager = new sepetManager();
+            sepetManager.Ekle(urun1);
+            sepetManager.Ekle(urun2);
+
+            sepetManager.Ekle2("Armut", "Yeşil armut", 12,10);
+            sepetManager.Ekle2("Elma", "Yeşil armut", 12,9);
+            sepetManager.Ekle2("Karpuz", "Diyarbakır Karpuzu", 12,8);
+
+
+        }
+    }
+}
+// Do not repeat yourself - DRY
+//Clean Code
+//Best Practice
